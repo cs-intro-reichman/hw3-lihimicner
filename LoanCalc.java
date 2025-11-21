6d93e2b8d2fd7c1794f6befc73e6af08b565f4ad
Lihi Micner <lihi.micner@post.runi.ac.il>
2025-11-21 16:07:17 +0200

    Completed assignment

--- LoanCalc.java
+++ LoanCalc.java
@@ -28,6 +28,7 @@ public class LoanCalc {
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
+		rate = rate / 100.0;
 		double balance = loan;
 		for (int i = 0; i < n; i++) {
 			balance = ((balance - payment) * (1.0 + rate));
